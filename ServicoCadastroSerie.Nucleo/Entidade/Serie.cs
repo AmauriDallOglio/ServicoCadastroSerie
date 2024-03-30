d444158333f8a0ed43fa08a9d9a5b709bc742ab7
Amauri Dall'Oglio <amauridalloglio@outlook.com.br>
2024-03-30 09:14:31 -0300

    Ajustes

--- ServicoCadastroSerie.Nucleo/Entidade/Serie.cs
+++ ServicoCadastroSerie.Nucleo/Entidade/Serie.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using static ServicoCadastroSerie.Nucleo.Enum.Enums;
+﻿using static ServicoCadastroSerie.Nucleo.Enum.Enums;
 
 
 namespace ServicoCadastroSerie.Nucleo.Entidade
--- ServicoCadastroSerie.Nucleo/Interface/IRepositorioGenerico.cs
+++ ServicoCadastroSerie.Nucleo/Interface/IRepositorioGenerico.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ServicoCadastroSerie.Nucleo.Interface
+﻿namespace ServicoCadastroSerie.Nucleo.Interface
 {
     public interface IRepositorioGenerico<T>
     {
